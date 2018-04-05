936a646c49a94dfb01a307728a84c20c9fee1208
Lev Novokreshchenov <xpr35@live.com>
2018-04-05 23:40:33 +0600

    fetching number of threads from args

--- src/main/java/com.xpr35.lunit/Runner.java
+++ src/main/java/com.xpr35.lunit/Runner.java
@@ -6,18 +6,26 @@ package com.xpr35.lunit;
  * Created by xpres on 16/03/18.
  */
 public class Runner {
+    private static int DEFAULT_THREADSNUMBER = 3;
 
     public static void main(String[] args) {
-        TestSuit testSuit = new TestSuit(3);
-        for (String s : args) {
+        int numberOfThreads = DEFAULT_THREADSNUMBER;
+        try {
+            numberOfThreads = Integer.valueOf(args[args.length - 1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Cannot parse number of threads param. Gonna use " +
+                    DEFAULT_THREADSNUMBER + " sthreads as default");
+        }
+        TestSuit testSuit = new TestSuit(numberOfThreads);
+        for (int i = 0; i < args.length - 1; i++) {
             try {
-                Class clazz = Class.forName(s);
+                Class clazz = Class.forName(args[i]);
                 testSuit.addClass(clazz);
             } catch (ClassNotFoundException e) {
-                System.err.println(e.getCause());
+                System.err.println("Cannot find class: " + e.getCause());
             }
         }
         testSuit.run();
-        System.out.println("\nTESTS:\n"+ testSuit.report());
+        System.out.println("\nTESTS:\n" + testSuit.report());
     }
 }
